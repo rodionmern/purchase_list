dd8b3d8c205a33daff9409bf683cb8bc9944e3d2
Rodion Mern <114694164+rodionmern@users.noreply.github.com>
2023-11-25 13:16:43 +0300

    ! Upload from localStorage working.

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,9 @@ var parent, inputPurchase, NewPurchase, ClosePurchase, NewTask;
 
 listPurchases = document.createElement('div');
 
+taska = 'taska';
+taskNumber = -1;
+
 function addNewPurchase() {
 
 	parent = document.querySelector('.purchases');
@@ -16,10 +19,10 @@ function addNewPurchase() {
 	ClosePurchase = document.createElement('li');
 	ClosePurchase.textContent = "X";
 
-	localStorage.setItem('Task', inputPurchase);
-	console.log(localStorage.getItem('Task'));
-
-	inputPurchase = document.getElementById('inputPurchase').value = "";
+	localStorage.setItem(taska, inputPurchase);
+	taskNumber = taskNumber + 1;
+	taska = 'taska' + `${taskNumber}`;
+	console.log(taska);
 
 	ClosePurchase.addEventListener( "click" , () => parent.removeChild(Element));
 
@@ -28,10 +31,43 @@ function addNewPurchase() {
 	NewPurchase.appendChild(NewTask);
 	NewPurchase.appendChild(ClosePurchase);
 
+	inputPurchase = document.getElementById('inputPurchase').value = "";
+
+}
+
+function returnLastPurchases() {
+
+	parent = document.querySelector('.purchases');
+
+	for (var i = 0; i < 5; i++) { // выведет 0, затем 1, затем 2
+		NewPurchase = document.createElement('ul');
+
+		inputPurchase = document.getElementById('inputPurchase').value;
+
+		NewTask = document.createElement('li');
+		NewTask.textContent = localStorage.getItem('taska' + i);
+
+		ClosePurchase = document.createElement('li');
+		ClosePurchase.textContent = "X";
+
+		parent.appendChild(listPurchases);
+		listPurchases.appendChild(NewPurchase);
+		NewPurchase.appendChild(NewTask);
+		NewPurchase.appendChild(ClosePurchase);
+	}
+
+	// document.getElementById('listPurchases').innerHTML = localStorage.getItem('taska');
+	// document.getElementById('listPurchases').innerHTML = localStorage.getItem('taska1');
+	// document.getElementById('listPurchases').innerHTML = localStorage.getItem('taska2');
+	// document.getElementById('listPurchases').innerHTML = localStorage.getItem('taska3');
+	// document.getElementById('listPurchases').innerHTML = localStorage.getItem('taska4');
+	// document.getElementById('listPurchases').innerHTML = localStorage.getItem('taska5');
+	// document.getElementById('listPurchases').innerHTML = localStorage.getItem('taska6');
+
 }
 
 function clearAllPurchases() {
-	
+
 	clearPurchases = document.getElementById('minus');
 
 	clearPurchases.addEventListener( "click" , () => parent.removeChild(listPurchases));
